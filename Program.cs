54260b5ab8e4c32c1de8e71d4879d303944bf14b
Nhan Duong <nhanduong@mail.usf.edu>
2020-01-30 13:42:22 -0500

    Writing codes to receive input from user then print out

--- Program.cs
+++ Program.cs
@@ -6,7 +6,11 @@ namespace UserInputNhanDuong
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input_string;
+            Console.WriteLine("Hi, this is Nhan Duong");
+            Console.WriteLine("Please type anything then press Enter to show it!");
+            input_string = Console.ReadLine();
+            Console.WriteLine("Got it! You have typed: " + input_string);
         }
     }
 }
