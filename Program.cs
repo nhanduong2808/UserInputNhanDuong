1f7aa72de25aebfdf20fd926c07ccefd089aaa91
Nhan Duong <nhanduong@mail.usf.edu>
2020-01-30 13:37:55 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserInputNhanDuong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
